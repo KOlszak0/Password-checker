eb7bbc86c0c978841eee55134d3dde77d0639793
Kacper <k.olszak25@gmail.com>
2025-07-29 08:39:19 +0200

    version with sha256 created

--- controllers/passwordController.js
+++ controllers/passwordController.js
@@ -4,9 +4,10 @@ const crypto = require('crypto')
 const generateHash = (req, res) => {
 	const password = req.body.password
 	const hash = crypto.createHash('sha256')
-	password.hash.digest(hex)
-	res.json({ password })
-	console.log('Password recieved', password)
+	hash.update(password)
+	const result = hash.digest('hex')
+	res.json({ hash: result })
+	console.log('Password recieved', result)
 }
 
 module.exports = { generateHash }
