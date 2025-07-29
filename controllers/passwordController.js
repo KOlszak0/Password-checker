e7653565cfc20e5a91ed37e7da76cf1bdaae6b86
Kacper <k.olszak25@gmail.com>
2025-07-29 09:32:46 +0200

    add zxcvbn password checker

--- controllers/passwordController.js
+++ controllers/passwordController.js
@@ -1,13 +1,16 @@
 const express = require('express')
 const bcrypt = require('bcrypt')
+const zxcvbn = require('zxcvbn')
 
 const saltRounds = 10
 
 const generateHash = async (req, res) => {
 	try {
 		const password = req.body.password
+		const { score, feedback, crack_times_display } = zxcvbn(password)
+
 		const hash = await bcrypt.hash(password, saltRounds)
-		res.json({ hash: hash })
+		res.json({ hash: hash, score: score, feedback: feedback, crack_times_display: crack_times_display })
 		console.log('Password recieved', hash)
 	} catch (err) {
 		console.error('Hashing error:', err)
