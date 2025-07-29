a802d20cb90c35d8e3efd83dded319cc928e5d0e
Kacper <k.olszak25@gmail.com>
2025-07-29 09:16:27 +0200

    change generateHash funtion to async

--- controllers/passwordController.js
+++ controllers/passwordController.js
@@ -4,18 +4,15 @@ const bcrypt = require('bcrypt')
 const saltRounds = 10
 
 const generateHash = async (req, res) => {
-	const password = req.body.password
-	bcrypt.hash(password, saltRounds, function (err, hash) {
-		try {
-			res.json({ hash: hash })
-			console.log('Password recieved', hash)
-		} catch {
-			console.log(err)
-		}
-	})
+	try {
+		const password = req.body.password
+		const hash = await bcrypt.hash(password, saltRounds)
+		res.json({ hash: hash })
+		console.log('Password recieved', hash)
+	} catch (err) {
+		console.error('Hashing error:', err)
+		res.status(500).json({ msg: 'Hashing failed' })
+	}
 }
-
-
-
 
 module.exports = { generateHash }
